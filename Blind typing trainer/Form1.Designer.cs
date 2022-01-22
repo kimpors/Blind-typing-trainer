5f966a0526ca82fbf0f8724bf00d891cf8a61631
Nikita <45769418+kimpors@users.noreply.github.com>
2022-01-22 17:11:32 +0200

    Створив пока бета версію фоми(мабуть)

--- Blind typing trainer/Form1.Designer.cs
+++ Blind typing trainer/Form1.Designer.cs
@@ -28,13 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.TypingField = new System.Windows.Forms.RichTextBox();
+            this.Start = new System.Windows.Forms.Button();
+            this.Time = new System.Windows.Forms.Label();
+            this.Speed = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TypingField
+            // 
+            this.TypingField.Location = new System.Drawing.Point(12, 12);
+            this.TypingField.Name = "TypingField";
+            this.TypingField.Size = new System.Drawing.Size(721, 239);
+            this.TypingField.TabIndex = 0;
+            this.TypingField.Text = "";
+            // 
+            // Start
+            // 
+            this.Start.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Start.Location = new System.Drawing.Point(12, 290);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(96, 38);
+            this.Start.TabIndex = 1;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            // 
+            // Time
+            // 
+            this.Time.AutoSize = true;
+            this.Time.Location = new System.Drawing.Point(53, 254);
+            this.Time.Name = "Time";
+            this.Time.Size = new System.Drawing.Size(49, 13);
+            this.Time.TabIndex = 2;
+            this.Time.Text = "00:00:00";
+            // 
+            // Speed
+            // 
+            this.Speed.AutoSize = true;
+            this.Speed.Location = new System.Drawing.Point(145, 255);
+            this.Speed.Name = "Speed";
+            this.Speed.Size = new System.Drawing.Size(13, 13);
+            this.Speed.TabIndex = 5;
+            this.Speed.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 254);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Time:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(104, 254);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Speed:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(745, 341);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Speed);
+            this.Controls.Add(this.Time);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.TypingField);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox TypingField;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Label Time;
+        private System.Windows.Forms.Label Speed;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
