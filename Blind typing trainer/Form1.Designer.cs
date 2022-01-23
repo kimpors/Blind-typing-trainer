61239831742dacc8ea437b9989c1eedb6ccb983b
Nikita <45769418+kimpors@users.noreply.github.com>
2022-01-23 21:00:34 +0200

    Бета версія

--- Blind typing trainer/Form1.Designer.cs
+++ Blind typing trainer/Form1.Designer.cs
@@ -32,14 +32,21 @@
             this.Start = new System.Windows.Forms.Button();
             this.Time = new System.Windows.Forms.Label();
             this.Speed = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // TypingField
             // 
+            this.TypingField.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.TypingField.DetectUrls = false;
+            this.TypingField.Enabled = false;
+            this.TypingField.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.TypingField.Location = new System.Drawing.Point(12, 12);
             this.TypingField.Name = "TypingField";
+            this.TypingField.ReadOnly = true;
+            this.TypingField.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Horizontal;
+            this.TypingField.ShowSelectionMargin = true;
             this.TypingField.Size = new System.Drawing.Size(721, 239);
             this.TypingField.TabIndex = 0;
             this.TypingField.Text = "";
@@ -68,41 +75,43 @@
             this.Speed.AutoSize = true;
             this.Speed.Location = new System.Drawing.Point(145, 255);
             this.Speed.Name = "Speed";
-            this.Speed.Size = new System.Drawing.Size(13, 13);
+            this.Speed.Size = new System.Drawing.Size(31, 13);
             this.Speed.TabIndex = 5;
-            this.Speed.Text = "0";
+            this.Speed.Text = "0WP";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 254);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Time:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 254);
+            this.label2.Location = new System.Drawing.Point(104, 254);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(33, 13);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Time:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(104, 254);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(41, 13);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "Speed:";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Speed:";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(745, 341);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.Speed);
             this.Controls.Add(this.Time);
             this.Controls.Add(this.Start);
             this.Controls.Add(this.TypingField);
+            this.KeyPreview = true;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Blind typing trainer";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -114,8 +123,8 @@
         private System.Windows.Forms.Button Start;
         private System.Windows.Forms.Label Time;
         private System.Windows.Forms.Label Speed;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
     }
 }
 
--- Blind typing trainer/Form1.cs
+++ Blind typing trainer/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace Blind_typing_trainer
 {
     public partial class Form1 : Form
@@ -15,6 +16,42 @@ namespace Blind_typing_trainer
         public Form1()
         {
             InitializeComponent();
+            TypingField.Text = "Привет как дела, Slimy sculpin sand tiger wolf-eel marlin tubeblenny, oceanic flyingfish, cookie-cutter shark porbeagle shark:Peter's";
+        }
+
+        private static int index = 0;
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            label1.Text = e.KeyChar.ToString();
+
+            if (TypingField.Text.Length == 0) return;
+
+            if (e.KeyChar == '\b')
+            {
+                if (index == 0) return;
+
+                TypingField.SelectionStart = index - 1;
+                TypingField.SelectionLength = index;
+                TypingField.SelectionBackColor = Color.GhostWhite;
+                index--;
+                return;
+            }
+
+            if (index == TypingField.Text.Length) return;
+
+            index++;
+            TypingField.SelectionStart = index - 1;
+            TypingField.SelectionLength = 1;
+
+            if (e.KeyChar != TypingField.Text[index - 1])
+                TypingField.SelectionBackColor = Color.Red;
+
+
+            if (e.KeyChar == TypingField.Text[index - 1])
+                TypingField.SelectionBackColor = Color.GreenYellow;
+
+
+            e.Handled = true;
         }
     }
 }
