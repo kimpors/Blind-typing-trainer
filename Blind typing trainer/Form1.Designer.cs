ac53b4745f068ca15542d803ef978ccec43a18ef
Nikita <45769418+kimpors@users.noreply.github.com>
2022-03-01 20:28:41 +0200

    1) Виправив мерехтіння курсора коли наводиш на Typingfield. 2) Додав темну і світлу тему 3) Додав кнопу рестарту 4) Додав зміну тексту кнопки старту

--- Blind typing trainer/Form1.Designer.cs
+++ Blind typing trainer/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.TypingField = new System.Windows.Forms.RichTextBox();
             this.Start = new System.Windows.Forms.Button();
             this.Time = new System.Windows.Forms.Label();
@@ -44,15 +45,16 @@
             this.raceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.StartTimer = new System.Windows.Forms.Label();
+            this.Theme = new System.Windows.Forms.Button();
+            this.Reset = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // TypingField
             // 
-            this.TypingField.AutoWordSelection = true;
+            this.TypingField.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.TypingField.Cursor = System.Windows.Forms.Cursors.Default;
             this.TypingField.DetectUrls = false;
-            this.TypingField.Enabled = false;
             this.TypingField.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.TypingField.Location = new System.Drawing.Point(0, 27);
             this.TypingField.Name = "TypingField";
@@ -63,13 +65,17 @@
             this.TypingField.TabIndex = 0;
             this.TypingField.TabStop = false;
             this.TypingField.Text = "";
+            this.TypingField.SelectionChanged += new System.EventHandler(this.TypingField_SelectionChanged);
+            this.TypingField.MouseMove += new System.Windows.Forms.MouseEventHandler(this.TypingField_MouseMove);
             // 
             // Start
             // 
             this.Start.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Start.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Start.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Start.Location = new System.Drawing.Point(12, 285);
             this.Start.Name = "Start";
-            this.Start.Size = new System.Drawing.Size(164, 24);
+            this.Start.Size = new System.Drawing.Size(174, 24);
             this.Start.TabIndex = 1;
             this.Start.TabStop = false;
             this.Start.Text = "Start";
@@ -79,36 +85,44 @@
             // Time
             // 
             this.Time.AutoSize = true;
-            this.Time.Location = new System.Drawing.Point(53, 269);
+            this.Time.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Time.ForeColor = System.Drawing.SystemColors.Menu;
+            this.Time.Location = new System.Drawing.Point(52, 269);
             this.Time.Name = "Time";
-            this.Time.Size = new System.Drawing.Size(49, 13);
+            this.Time.Size = new System.Drawing.Size(49, 14);
             this.Time.TabIndex = 2;
             this.Time.Text = "00:00:00";
             // 
             // Speed
             // 
             this.Speed.AutoSize = true;
-            this.Speed.Location = new System.Drawing.Point(145, 270);
+            this.Speed.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Speed.ForeColor = System.Drawing.SystemColors.Menu;
+            this.Speed.Location = new System.Drawing.Point(155, 268);
             this.Speed.Name = "Speed";
-            this.Speed.Size = new System.Drawing.Size(31, 13);
+            this.Speed.Size = new System.Drawing.Size(31, 14);
             this.Speed.TabIndex = 5;
             this.Speed.Text = "0WP";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 269);
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Menu;
+            this.label1.Location = new System.Drawing.Point(11, 269);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.Size = new System.Drawing.Size(35, 14);
             this.label1.TabIndex = 6;
             this.label1.Text = "Time:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(104, 269);
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Menu;
+            this.label2.Location = new System.Drawing.Point(107, 269);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.Size = new System.Drawing.Size(42, 14);
             this.label2.TabIndex = 7;
             this.label2.Text = "Speed:";
             // 
@@ -139,7 +153,7 @@
             // openToolStripMenuItem
             // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.openToolStripMenuItem.Text = "Open";
             this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
             // 
@@ -172,16 +186,46 @@
             // 
             this.StartTimer.AutoSize = true;
             this.StartTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.StartTimer.Location = new System.Drawing.Point(182, 269);
+            this.StartTimer.ForeColor = System.Drawing.SystemColors.Menu;
+            this.StartTimer.Location = new System.Drawing.Point(192, 269);
             this.StartTimer.Name = "StartTimer";
             this.StartTimer.Size = new System.Drawing.Size(39, 42);
             this.StartTimer.TabIndex = 9;
             this.StartTimer.Text = "3";
             // 
+            // Theme
+            // 
+            this.Theme.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Theme.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Theme.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Theme.Image = ((System.Drawing.Image)(resources.GetObject("Theme.Image")));
+            this.Theme.Location = new System.Drawing.Point(237, 272);
+            this.Theme.Name = "Theme";
+            this.Theme.Size = new System.Drawing.Size(31, 39);
+            this.Theme.TabIndex = 10;
+            this.Theme.UseVisualStyleBackColor = true;
+            this.Theme.Click += new System.EventHandler(this.Theme_Click);
+            // 
+            // Reset
+            // 
+            this.Reset.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Reset.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Reset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Reset.Image = ((System.Drawing.Image)(resources.GetObject("Reset.Image")));
+            this.Reset.Location = new System.Drawing.Point(284, 272);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(31, 39);
+            this.Reset.TabIndex = 11;
+            this.Reset.UseVisualStyleBackColor = true;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
             // Form1
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(742, 313);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.Theme);
             this.Controls.Add(this.StartTimer);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -190,6 +234,7 @@
             this.Controls.Add(this.Start);
             this.Controls.Add(this.TypingField);
             this.Controls.Add(this.menuStrip1);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.KeyPreview = true;
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
@@ -219,6 +264,8 @@
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.RichTextBox TypingField;
         private System.Windows.Forms.Label StartTimer;
+        private System.Windows.Forms.Button Theme;
+        private System.Windows.Forms.Button Reset;
     }
 }
 
--- Blind typing trainer/Form1.cs
+++ Blind typing trainer/Form1.cs
@@ -8,26 +8,94 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
 
 
 namespace Blind_typing_trainer
 {
     public partial class Form1 : Form
     {
-        private int symbolCount = 0, mistake;
+        private int symbolCount = 0, mistake = 0;
+        private bool isLight = true;
         private TimeSpan timer = new TimeSpan(0, 0, 0);
         public Form1()
         {
             InitializeComponent();
             TypingField.Text = "Text here!";
+            Start.FlatAppearance.BorderSize = 0;
+            Theme.FlatAppearance.BorderSize = 0;
+            Reset.FlatAppearance.BorderSize = 0;
+            LightTheme();
         }
+
+        private void currTheme()
+        {
+            if (isLight) LightTheme();
+            else DarkTheme();
+        }
+        private void LightTheme()
+        {
+            menuStrip1.BackColor = Color.FromArgb(159, 167, 255);
+            menuStrip1.ForeColor = Color.FromArgb(193, 196, 226);
+
+            Start.BackColor = Color.FromArgb(202, 207, 255);
+            Start.ForeColor = Color.FromArgb(66, 72, 128);
+
+            TypingField.BackColor = Color.FromArgb(202, 207, 255);
+            Restart();
+
+            Time.ForeColor = Color.FromArgb(66, 72, 128);
+            Speed.ForeColor = Color.FromArgb(66, 72, 128);
+            StartTimer.ForeColor = Color.FromArgb(66, 72, 128);
+            label1.ForeColor = Color.FromArgb(66, 72, 128);
+            label2.ForeColor = Color.FromArgb(66, 72, 128);
+
+
+
+            BackColor = Color.FromArgb(159, 167, 255);
+            ForeColor = Color.FromArgb(66, 72, 128);
+        }
+        private void DarkTheme()
+        {
+            menuStrip1.BackColor = Color.FromArgb(45, 48, 78);
+            menuStrip1.ForeColor = Color.FromArgb(193, 196, 226);
+
+
+            Start.BackColor = Color.FromArgb(77, 80, 112);
+            Start.ForeColor = Color.FromArgb(193, 196, 226);
+
+            Theme.BackColor = Color.FromArgb(45, 48, 78);
+
+            TypingField.BackColor = Color.FromArgb(77, 80, 112);
+            Restart();
+
+            Time.ForeColor = Color.FromArgb(193, 196, 226);
+            Speed.ForeColor = Color.FromArgb(193, 196, 226);
+            StartTimer.ForeColor = Color.FromArgb(193, 196, 226);
+            label1.ForeColor = Color.FromArgb(193, 196, 226);
+            label2.ForeColor = Color.FromArgb(193, 196, 226);
+
+
+            BackColor = Color.FromArgb(45, 48, 78);
+            ForeColor = Color.FromArgb(193, 196, 226);
+        }
+
+        private void Restart()
+        {
+            TypingField.SelectAll();
+
+            if (isLight)
+                TypingField.SelectionColor = Color.FromArgb(66, 72, 128);
+            else
+                TypingField.SelectionColor = Color.FromArgb(193, 196, 226);
+        }
+
         private void Form1_KeyPress(object sender, KeyPressEventArgs e)
         {
-            label1.Focus();
             if (StartTimer.Text == "0")
             {
                 TypingField.SelectionLength = 1;
-
 
                 if (e.KeyChar == '\b')
                 {
@@ -45,7 +113,9 @@ namespace Blind_typing_trainer
                 TypingField.SelectionStart = symbolCount;
 
                 if (e.KeyChar == TypingField.Text[symbolCount])
+                {
                     TypingField.SelectionColor = Color.LimeGreen;
+                }  
                 else
                 {
                     mistake++;
@@ -54,6 +124,7 @@ namespace Blind_typing_trainer
 
 
                 symbolCount++;
+
                 if (symbolCount == TypingField.Text.Length)
                 {
                     timerTick.Enabled = false;
@@ -71,6 +142,7 @@ namespace Blind_typing_trainer
         {
             timerTick.Enabled = !timerTick.Enabled;
             StartTimer.Text = "3";
+            Start.Text = timerTick.Enabled && symbolCount != TypingField.Text.Length ? "Stop" : "Start";
         }
 
         private void timer_Tick(object sender, EventArgs e)
@@ -82,12 +154,37 @@ namespace Blind_typing_trainer
                 timer = timer.Add(new TimeSpan(0, 0, 1));
                 Time.Text = timer.ToString();
 
-                Speed.Text = ((symbolCount + mistake) / (timer.TotalMinutes) / 5).ToString(".00");
+                Speed.Text = ((symbolCount + mistake) / (timer.TotalMinutes) / 5).ToString();
             }
             else
             {
                 StartTimer.Text = (Convert.ToInt16(StartTimer.Text) - 1).ToString();
             }
+        }
+        private void Theme_Click(object sender, EventArgs e)
+        {
+            isLight = !isLight;
+            currTheme();
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            symbolCount = mistake = 0;
+            Restart();
+        }
+
+        private void TypingField_SelectionChanged(object sender, EventArgs e)
+        {
+            label1.Focus();
+        }
+
+        private void TypingField_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!TypingField.ClientRectangle.Contains(e.Location))
+                TypingField.Capture = false;
+
+            else if (!TypingField.Capture)
+                TypingField.Capture = true;
         }
 
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
@@ -103,7 +200,7 @@ namespace Blind_typing_trainer
                     TypingField.LoadFile(openFileDialog1.FileName);
                 }
 
-                symbolCount = mistake = -1;
+                symbolCount = mistake = 0;
             }
         }
     }
